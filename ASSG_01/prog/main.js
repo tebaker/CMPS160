7948d970132408b9e6852a0c9e56d96a8d250d33
Talon Baker <taalonn@gmail.com>
2018-10-02 16:11:19 -0700

    got a canvas set up. Set rect fill color to black

--- ASSG_01/prog/main.js
+++ ASSG_01/prog/main.js
@@ -2,7 +2,20 @@
  * Function called when the webpage loads.
  */
 function main() {
-  //
-  // YOUR CODE HERE
-  //
+  // Retrieve <canvas> element
+  let canvas = document.getElementById('example');
+  if (!canvas) {
+  	console.log("Failed to retrieve the <canvas> element");
+  	return;
+  }
+
+  // Get the rendering context for 2DCG
+  let ctx = canvas.getContext('2d');
+
+  // Draw a black rectangle
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+
+  // Setting the 'fill' from corner to corner
+  ctx.fillRect(0, 0, 500, 500);
+
 }
