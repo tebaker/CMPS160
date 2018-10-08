42b51c89b5683e3ff4708878d116306236976d43
Talon Baker <taalonn@gmail.com>
2018-10-08 13:50:04 -0700

    replaced all code with working ASSG1 code as a base. Will modify from that with new code.

--- ASSG_02/prog/glslFunctions.js
+++ ASSG_02/prog/glslFunctions.js
@@ -1,49 +1,13 @@
 /**
- * Sends data to an attribute variable using a buffer.
- *
- * @private
- * @param {Float32Array} data Data being sent to attribute variable
- * @param {Number} dataCount The amount of data to pass per vertex
- * @param {String} attribName The name of the attribute variable
- */
-function sendAttributeBufferToGLSL(data, dataCount, attribName) {
-  //
-  // YOUR CODE HERE
-  //
-
-  // Recommendations: This piece of code should do these three things:
-  // 1. Create a an attribute buffer
-  // 2. Bind data to that buffer
-  // 3. Enable the buffer for use
-  //
-  // Some modifications can be made to this function to improve performance. Ask
-  // a TA in lab if you're interested in these modifications.
-}
-
-/**
- * Draws the current buffer loaded. Buffer was loaded by sendAttributeBufferToGLSL.
- *
- * @param {Integer} pointCount The amount of vertices being drawn from the buffer.
- */
-function tellGLSLToDrawCurrentBuffer(pointCount) {
-  //
-  // YOUR CODE HERE
-  //
-
-  // Recommendations: Should only be one line of code.
-}
-
-/**
  * Sends a float value to the specified uniform variable within GLSL shaders.
  * Prints an error message if unsuccessful.
  *
  * @param {float} val The float value being passed to uniform variable
  * @param {String} uniformName The name of the uniform variable
  */
-function sendUniformFloatToGLSL(val, uniformName) {
-  //
-  // YOUR CODE HERE
-  //
+function sendUniformFloatToGLSL(gl, val, uniformName) {
+	// Pass vertex position to attribute variable
+    gl.vertexAttrib3f(uniformName, val[0], val[1], val[2]);
 }
 
 /**
@@ -53,8 +17,7 @@ function sendUniformFloatToGLSL(val, uniformName) {
  * @param {Array} val Array (vector) being passed to uniform variable
  * @param {String} uniformName The name of the uniform variable
  */
-function sendUniformVec4ToGLSL(val, uniformName) {
-  //
-  // YOUR CODE HERE
-  //
+function sendUniformVec4ToGLSL(gl, val, uniformName) {
+    // Pass color uniform to fragment shader
+    gl.uniform4f(uniformName, val[0], val[1], val[2], val[3]);
 }
