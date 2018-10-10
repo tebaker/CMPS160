a3a40c5c0f4295e7c50b0aea39e085891dae7d04
Talon Baker <taalonn@gmail.com>
2018-10-09 19:38:59 -0700

    started working with the classes.

--- ASSG_02/prog/scene/geometry.js
+++ ASSG_02/prog/scene/geometry.js
@@ -5,25 +5,40 @@
  * @this {Geometry}
  */
 class Geometry {
-  /**
-   * Constructor for Geometry.
-   *
-   * @constructor
-   */
-  constructor() {
-    this.vertices = []; // Vertex objects. Each vertex has x-y-z.
-    this.color = [];  // The color of your geometric object
-  }
+	/**
+	* Constructor for Geometry.
+	*
+	* @constructor
+	*/
+	constructor() {
+		this.centerPoint = {x: 0.0, y: 0.0}; // Vertex objects. Each vertex has x-y-z
+		this.color = {r: 0.0, g: 0.0, b: 0.0};  // The color of your geometric object
+		this.size = 0.0; // Size length or radius of object
+	}
 
-  /**
-   * Renders this Geometry within your webGL scene.
-   */
-  render() {
-    //
-    // YOUR CODE HERE
-    //
+	setCenterPoint(centerX, centerY) {
+		this.centerPoint = {x: centerX, y: centerY};
+	}
 
-    // Recommendations: sendUniformVec4ToGLSL(), tellGLSLToDrawCurrentBuffer(),
-    // and sendAttributeBufferToGLSL() are going to be useful here.
-  }
-}
+	setColor(rVal, gVal, bVal) {
+		this.color = {r: rVal, g: gVal, b: bVal};
+	}
+	
+	setSize(sizeVal) {
+		this.size = sizeVal;
+	}
+
+
+
+	/**
+	* Renders this Geometry within your webGL scene.
+	*/
+	render() {
+	//
+	// YOUR CODE HERE
+	//
+
+	// Recommendations: sendUniformVec4ToGLSL(), tellGLSLToDrawCurrentBuffer(),
+	// and sendAttributeBufferToGLSL() are going to be useful here.
+	}
+	}
